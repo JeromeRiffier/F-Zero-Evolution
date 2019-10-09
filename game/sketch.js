1b985fb18c9b1a10a4c71acaa04825ac39eb0145
JeromeRiffier <53470235+JeromeRiffier@users.noreply.github.com>
2019-10-09 14:32:47 +0200

    Add files via upload
    Ajout de chose a faire et a améliorer

--- game/sketch.js
+++ game/sketch.js
@@ -1,3 +1,43 @@
+/*  Reste a faire 
+
+    - Creer le systeme de recompence via le score des pilotes
+        Plus il tienne lomgtemps
+        Plus il vont vite d'un checkpoint a un autre
+        Meilleur sera le score
+
+    - Ajouter un timer de limite de fin
+        5min a partir du moment ou le premier a finis sont premier tour ?
+        ou
+        15min a partir du début de la course ?
+
+    - Préparer le systeme de reproduction mutation 
+        Comment sélectionner les meilleurs ? combien on en garde ? 
+
+
+    - Préparer le systeme de compétition pour permettre de refaire des course avec les joueurs des generation succesive
+
+    - Ajouter une interface permettant de choisir le nombre de pilote/gen et autre parametre fort utile
+
+
+*/
+/*   Amélioration faisable 
+
+
+    - Modifier la gestion de l'acceleration par l'IA
+        Plutot que convertir l'output entre 0 et 1 de façon lineaire vers un range entre 0 et 0.2
+        Peut être utilisé une courbe serais plus efficasse ?
+
+    - Amelioré le calcul des  distance aux obstacle:
+        Pour l'instant on test si les "vison_ray" touches chacque mur.
+        Il dois y avoir moyen d'en prendre moin en compte.. (si ça à touché un bord exterieur ne pas calculé les bord interieur deja^^)
+        De plus modifier la valeur fourni a l'ia, en fournissant la distance aux point d'impact des "vision_ray" plutot que leur position ?
+
+
+*/
+
+
+
+
 let running = false;
 let cars = [];
 
